2f81e0c5b0d5cad0cb0f87a4fedc463d80b4cca6
Keshavarz <kak136@usask.ca>
2021-03-26 12:13:31 -0600

    transfering to an organization and changing the folder names

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="vars-tool-kasra545", # Replace with your own username
+    version="2.1",
+    author="Saman Razavi",
+    author_email="saman.razavi@usask.ca",
+    description="The Python version of the VARS-TOOL sensitivity and uncertainty analysis package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/vars-tool/vars-tool/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL3 License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
