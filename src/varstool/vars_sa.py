db92b55dfc81d6216b9c65d3523842807250c40a
Cordell Blanchard <CordellBlanchard@gmail.com>
2021-06-14 18:19:39 -0400

    Added minor changes and finished bootstrapping for IVARS

--- src/varstool/vars_sa.py
+++ src/varstool/vars_sa.py
@@ -55,15 +55,15 @@ class Model(Protocol):
     __doc__ = """A wrapper class for a function of interest"""
 
     def __init__(
-        self, 
+        self,
         func: Callable,
         unkown_options: Dict[str, str] = None,
     ) -> None:
-        
+
         # check whether the input is a callable
         assert callable(func)
         self.func = func
-        
+
         # unkown_options must be a dict
         assert isinstance(unkown_options, dict)
         if unknown_options:
@@ -211,12 +211,14 @@ class VARS(object):
             raise ValueError(
                 "`sampler` algorithm must be of type varstool.Sampler."
             )
+        self.sampler = sampler
 
         ### `model`
         if not isinstance(model, Model):
             raise ValueError(
                 "`model` must be of type varstool.Model."
             )
+        self.model = model
 
         # adding anything else here?!
 
@@ -248,8 +250,8 @@ class VARS(object):
 
     #-------------------------------------------
     # Core properties
-    
-    ## using dunder variables for avoiding confusion with 
+
+    ## using dunder variables for avoiding confusion with
     ## D-/GVARS sublcasses.
 
     @property
@@ -287,7 +289,7 @@ class VARS(object):
     def generate_star(star_centres, delta_h, param_names=[]):
 
         # generate star points using star.py functions
-        star_points = sv.star_vars(star_centres, delta_h=delta_h, parameters=param_names, rettype='DataFrame')
+        star_points = sv.star(star_centres, delta_h=delta_h, parameters=param_names, rettype='DataFrame')
 
         # figure out way to return this?
         return star_points  # for now will just do this
@@ -301,72 +303,67 @@ class VARS(object):
         pass
 
 
-    def run_online(self, ):
-        # create instance of sampler to get star centers (random numbers)
-        sample = Sampler(self.sampler, self.parameters)  # this might not be needed?
-        self.centres(sample())
+    # param_names is the name of the model parameters might need a better name
+    def run_online(self, param_names=[]):
+        # call sampler to get star centres
+        self.centres(sampler())
 
         # generate star points
         self.points(sv.star(star_centres=self.centres(), delta_h=self.delta_h, parameters=param_names, rettype='DataFrame'))
 
         # apply model to the generated star points
-        df = vf.apply_unique(self.model, self.points())
+        df = vf.apply_unique(self.model(), self.points())
         df.index.names = ['centre', 'param', 'points']
 
-        # possibly do some data manipulation here not sure yet
-
         # get paired values for each section based on 'h'
-        pair_df = df[self.model.__name__].groupby(level=[0,1]).apply(vf.section_df)
+        pair_df = df[self.model.repr()].groupby(level=[0,1]).apply(vf.section_df)
         pair_df.index.names = ['centre', 'param', 'h', 'pair_ind']
 
-        # get mu_star value?
-        mu_star_df = df[self.model.__name__].groupby(level=[0,1]).mean()
+        # get mu_star value
+        mu_star_df = df[self.model.repr()].groupby(level=[0,1]).mean()
         mu_star_df.index.names = ['centre', 'param']
         mu_star_df.unstack(level=1)
 
         # overall mean of the unique evaluated function value over all star points
-        mu_overall = df[self.model.__name__].unique().mean()
+        mu_overall = df[self.model.repr()].unique().mean()
 
         # overall variance of the unique evaluated function over all star points
-        var_overall = df[self.model.__name__].unique().var(ddof=1)
-
-        # also possibly add data manipulation to pair_df and mu_star_df not sure
+        var_overall = df[self.model.repr()].unique().var(ddof=1)
 
         # sectional covariogram calculation
         cov_section_all = vf.cov_section(pair_df, mu_star_df)
         cov_section_all.unstack(level=1)
 
         # variogram calculation
-        variogram_value = vf.variogram(pair_df)
-        variogram_value.unstack(level=0)
+        self.variogram_value = vf.variogram(pair_df)
+        self.variogram_value.unstack(level=0)
 
         # morris calculation
-        morris_values = vf.morris_eq(pair_df)
-        morris_values[0].unstack(level=0)
+        self.morris_values = vf.morris_eq(pair_df)
+        self.morris_values[0].unstack(level=0)
 
         # overall covariogram calculation
-        covariogram_value = vf.covariogram(pair_df, mu_overall)
-        covariogram_value.unstack(level=0)
+        self.covariogram_value = vf.covariogram(pair_df, mu_overall)
+        self.covariogram_value.unstack(level=0)
 
         # expected value of the overall covariogram calculation
-        e_covariogram_value = vf.e_covariogram(cov_section_all)
-        e_covariogram_value.unstack(level=0)
+        self.e_covariogram_value = vf.e_covariogram(cov_section_all)
+        self.e_covariogram_value.unstack(level=0)
 
         # sobol calculation
-        sobol_value = vf.sobol_eq(variogram_value, e_covariogram_value, var_overall)
+        self.sobol_value = vf.sobol_eq(self.variogram_value, self.e_covariogram_value, var_overall)
 
         # IVARS calculation
-        ivars_df = pd.DataFrame.from_dict({scale: variogram_value.groupby(level=0).apply(vf.ivars, scale=scale, delta_h=self.delta_h) \
+        self.ivars_df = pd.DataFrame.from_dict({scale: self.variogram_value.groupby(level=0).apply(vf.ivars, scale=scale, delta_h=self.delta_h) \
              for scale in self.ivars_scales}, 'index')
 
-        # create result data frames for if bootstrapping is chosen to be done (might be better way to do this)
-        result_bs_variogram = pd.DataFrame()
-        result_bs_ecovariogram = pd.DataFrame()
-        result_bs_var = pd.DataFrame()
-        result_bs_sobol = pd.DataFrame()
-        result_bs_ivars_df = pd.DataFrame()
 
         if self.bootstrap_flag:
+            # create result dataframes/series if bootstrapping is chosen to be done
+            result_bs_variogram = pd.Series(dtype='float64')
+            result_bs_sobol = pd.Series(dtype='float64')
+            result_bs_ivars_df = pd.DataFrame()
+
             for iter in range(0, self.bootstrap_size):
                 # bootstrapping to get CIs
                 # specify random sequence by sampling with replacement
@@ -386,61 +383,34 @@ class VARS(object):
 
                 bootstrapped_sobol = vf.sobol_eq(bootstrapped_variogram, bootstrapped_ecovariogram, bootstrapped_var)
 
-                ivars_values = [0.1, 0.3, 0.5]
-                delta_h = 0.1
                 bootstrapped_ivars_df = pd.DataFrame.from_dict(
-                    {scale: bootstrapped_variogram.groupby(level=0).apply(vf.ivars, scale=scale, delta_h=delta_h) \
-                     for scale in ivars_values}, 'index')
-                # gamma (for reference delete these comments later)
-                result_bs_variogram.append(bootstrapped_variogram)
-                # Ecov
-                result_bs_ecovariogram.append(bootstrapped_ecovariogram)
-                # var samples
-                result_bs_var.append(bootstrapped_var)
-                #ST
-                result_bs_sobol.append(bootstrapped_sobol)
-                #IVARS
-                result_bs_ivars_df.append(bootstrapped_ivars_df)
+                    {scale: bootstrapped_variogram.groupby(level=0).apply(vf.ivars, scale=scale, delta_h=self.delta_h) \
+                     for scale in self.ivars_scales}, 'index')
+
+                # attatch new results to previous results (order does not matter here)
+                result_bs_variogram = pd.concat([bootstrapped_variogram, result_bs_variogram])
+                result_bs_sobol = pd.concat([bootstrapped_sobol, result_bs_sobol])
+                result_bs_ivars_df = pd.concat([bootstrapped_ivars_df, result_bs_ivars_df])
+
+            # need to do data manipulation to variogram and sobol results
+            # in order sort all the parameters independently
+            #TODO
 
             # sort the bootstrapping results
-            result_bs_variogram.sort()
-            result_bs_ecovariogram.sort()
-            result_bs_var.sort()
-            result_bs_sobol.sort()
-            result_bs_ivars_df.sort()
+            result_bs_variogram.sort_values([param_names])
+            result_bs_sobol.sort_values([param_names])
+            result_bs_ivars_df.sort_values([param_names])
 
-            # calculating the confidence interval limits
-            # variogram_upp = result_bs_variogram*
-            # variogram_low =
+            # calculating the confidence interval limits for variogram/sobol results
+            self.variogram_low = result_bs_variogram.iloc[self.bootstrap_size*(1-self.bootstrap_ci)/2]
+            self.variogram_upp = result_bs_variogram.iloc[self.bootstrap_size*(1 - ((1 - self.bootstrap_ci)/2))]
 
-            # sobol_upp =
-            # sobol_low =
+            self.sobol_low = result_bs_sobol.iloc[self.bootstrap_size*(1-self.bootstrap_ci)/2]
+            self.sobol_upp = result_bs_sobol.iloc[self.bootstrap_size*(1 - ((1 - self.bootstrap_ci)/2))]
 
-            # ivars_low =
-            # ivars_upp =
-
-
-            # return results in a dictionary ***not finished yet also not sure if it should be a dict
-            return {
-                'Directional_Variogram': 1,
-                'Directional_Covariogram': 1,
-                'Directional_Expected_Covariogram': 1,
-                'Integrated_Variogram': 1,
-                'Factor_Rankings(IVARS)': 1,
-                'VARS-TO': 1,
-                'Factor_Rankings(VARS_TO)': 1,
-                'VARS-ABE': 1,
-                'VARS-ACE': 1,
-                'gamma_LL': 1,
-                'gamma_UL': 1,
-                'IVARS_LL': 1,
-                'IVARS_UL': 1,
-                'VARS-TO_LL': 1,
-                'VARS-TO_UL': 1,
-                'Reliability_Estimates(IVARS)': 1,
-                'Factor_Grouping(VARS-TO/IVARS50)': 1,
-                'Reliability_Estimates(Factor_Grouping)': 1
-            }
+            # calculate upper and lower confidence interval limits of the ivars values
+            self.ivars_low = result_bs_ivars_df.quantile((1-self.bootstrap_ci)/2)
+            self.ivars_upp = result_bs_ivars_df.quantile(1-((1-self.bootstrap_ci)/2))
 
 
     def run_offline(star_points,):
