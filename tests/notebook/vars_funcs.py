87ed85126a5b63da752fc99e87fd4a7ca4eb6a40
Keshavarz <kak136@usask.ca>
2021-07-06 08:40:16 -0600

    VARS-TS nearly completed - bootstrapping missing

---
+++ tests/notebook/vars_funcs.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+from itertools import combinations
+
+
+def ishigami(x, a=7, b=0.05):
+    '''Ishigami test function'''
+    # check whether the input x is an array-like
+
+    if not isinstance(x, (pd.core.frame.DataFrame, pd.core.series.Series, np.ndarray, list)):
+        raise TypeError('`x` must be of type pandas.DataFrame, numpy.ndarray, pd.Series, or list')
+
+    if x.shape[0] > 3:
+        raise ValueError('`x` must have only three arguments at a time')
+    elif x.shape[0] <3:
+        raise ValueError('`x` must have three arguments passed in an array-like object')
+
+    return np.sin(x[0]) + a*(np.sin(x[1])**2) + b*(x[2]**4)*np.sin(x[0])
+
+
+# helper functions
+def apply_unique(func, df, axis=1, *args, **kwargs):
+    '''Apply a function to unique rows of a DataFrame
+    for efficiency.'''
+
+    applied_df = df.merge(df.drop_duplicates()
+                         .assign(**{str(func): lambda x: x.apply(func, axis=axis)}),
+                         how='left')
+    applied_df.index = df.index
+
+    return applied_df
+
+
+def pairs_h(iterable):
+    '''gives the pairs of numbers considering their differences'''
+    interval = range(min(iterable), max(iterable)-min(iterable))
+    pairs  = {key+1:[j for j in combinations(iterable, 2) if np.abs(j[0]-j[1])==key+1] for key in interval}
+    return pairs
+
+
+def section_df(df):
+    '''gets the paired values of each section based on index'''
+    pairs = pairs_h(df.index.get_level_values(-1))
+    df_values = df.to_numpy()
+    sample = pd.concat({h:
+                    pd.DataFrame.from_dict({str(idx_tup): [df_values[idx_tup[0]], df_values[idx_tup[1]]] for idx_tup in idx}, 'index') \
+                      for h, idx in pairs.items()})
+
+    return sample
+
+
+# lambda functions
+'''covariogram of each section'''
+cov_section = lambda pair_cols, mu_star: (pair_cols.sub(mu_star, axis=0)[0] * pair_cols.sub(mu_star, axis=0)[1]).groupby(level=[0,1,2]).mean()
+
+'''variogram over all sections'''
+variogram = lambda pair_cols: 0.5*(pair_cols[0] - pair_cols[1]).pow(2).groupby(level=[1,2]).mean()
+
+'''morris sensitivity measure equivalent evaluated over all sections'''
+morris_eq = lambda pair_cols: ((pair_cols[1] - pair_cols[0]).abs().groupby(level=[1,2]).mean(), \
+                               (pair_cols[1] - pair_cols[0]).groupby(level=[1,2]).mean())
+
+'''covariogram over all sections'''
+covariogram = lambda pair_cols, mu_overall: ((pair_cols - mu_overall)[0] * (pair_cols - mu_overall)[1]).groupby(level=[1,2]).mean()
+
+'''expected covariogram over all sections'''
+e_covariogram = lambda cov_section_all: cov_section_all.groupby(level=[1,2]).mean()
+
+'''sobol (total order) sensitivity measure equivalent evaluated over all sections'''
+sobol_eq = lambda gamma, ecov, variance: ((gamma + ecov) / variance).loc[:,1]
+
+
+# ivars function
+def ivars(variogram_array, scale, delta_h):
+    '''generate Integrated Variogram Across a Range of Scales (IVARS)
+    by approximating area using right trapezoids having width of `delta_h`
+    and hights of variogram values'''
+    num_h  = len(variogram_array.index.levels[-1].to_list())
+    x_bench= np.arange(start=0, stop=delta_h*(num_h+1), step=delta_h)
+    x_int  = np.arange(start=0, stop=(scale*10+1)/10, step=delta_h)
+
+    # calculate interpolated values for both x (h) and y (variogram)
+    if x_int[-1] < scale:
+        x_int.append(scale)
+    y_bench= [0] + variogram_array.to_list()
+
+    y_int  = np.interp(x=x_int, xp=x_bench, fp=y_bench)
+
+    # for loop for each step size to caluclate the area
+    ivars = 0
+    for i in range(len(x_int)-1):
+        ivars += 0.5*(y_int[i+1] + y_int[i]) * (x_int[i+1] - x_int[i])
+
+    return ivars
