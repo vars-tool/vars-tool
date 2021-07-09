34d4272d5f99a2189935290a5bf36c3ddc84e2bb
Kasra Keshavarz <kasra.keshavarz@usask.ca>
2021-07-09 04:15:42 -0600

    testing TSVARS

--- src/varstool/funcs/hbv.py
+++ src/varstool/funcs/hbv.py
@@ -66,7 +66,7 @@ def HBV_SASK(par_values=[], basin='banff'):
     state['S1'] = S1
     state['S2'] = S2
 
-    return flux['Q_cms'][1000:]
+    return flux['Q_cms'][365:]
 
 
 def run_model(par_values,watershed_area,ini_values,forcing,long_term):
