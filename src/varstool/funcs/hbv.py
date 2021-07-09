bd6137479e4f9758ae33b9f52d4ec32c33cf35c4
Kasra Keshavarz <kasra.keshavarz@usask.ca>
2021-07-09 02:49:01 -0600

    testing TSVARS

--- src/varstool/funcs/hbv.py
+++ src/varstool/funcs/hbv.py
@@ -1,3 +1,7 @@
+import os
+
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 #import saveload as sl
@@ -8,22 +12,22 @@ from IPython.display import clear_output
 def read_inputs(data_folder):
 
     inp={}
-
+path =  "../data/test.csv"
     # % ********  Initial Condition  *********
-    fn='./'+data_folder + '/initial_condition.inp'
+    fn=Path(__file__).parent / './'+data_folder + '/initial_condition.inp'
     data=np.loadtxt(fn,delimiter=' ',usecols=[1])
     watershed_area=data[0]
     ini_values=data[1:]
 
     # % ********  Precipitation and Temperature  *********
-    fn='./'+data_folder + '/Precipitation_Temperature.inp'
+    fn=Path(__file__).parent /'./'+data_folder + '/Precipitation_Temperature.inp'
     forcing=pd.read_csv(fn,delim_whitespace=True,index_col=0,parse_dates=True,names=['P','T'])
     #forcing=pd.read_csv(fn,delim_whitespace=True,usecols=[1,2],names=['P','T'])
     #forcing.index=pd.date_range(start='1979-01-01',freq='D',periods=len(forcing))
     forcing['month_time_series']=forcing.index.month.values
 
     # % ********  Evapotranspiration  *********
-    fn='./'+data_folder + '/monthly_data.inp'
+    fn=Path(__file__).parent /'./'+data_folder + '/monthly_data.inp'
     Tave,PEave=np.loadtxt(fn,delimiter='\t',unpack=True)
     long_term={}
     long_term['monthly_average_T']=Tave
