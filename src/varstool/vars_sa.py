564796cda34c6dab48e3938167a9f1a2efbd9262
Kasra Keshavarz <kasra.keshavarz@usask.ca>
2021-07-09 04:40:24 -0600

    testing TSVARS

--- src/varstool/vars_sa.py
+++ src/varstool/vars_sa.py
@@ -1152,15 +1152,22 @@ class TSVARS(VARS):
                       for scale in self.ivars_scales}, 'index')
                     self.ivars = pd.concat([self.ivars_values, temp_ivars_values])
 
+                    self.run_status = True
+
             else:
                 # pair_df is built serially - other functions are the same as parallel
+                print('here 1')
                 self.pair_df = df.groupby(level=0, axis=1).apply(ts_pair)
                 self.pair_df.index.names = ['centre', 'param', 'h', 'pair_ind']
                 self.pair_df.columns.names = ['ts', None]
-                self.pair_df.stack(level=0).reorder_levels([-1,0,1,2,3]).sort_index()
+                self.pair_df.stack(level='ts').reorder_levels([-1,0,1,2,3]).sort_index()
+
+                print('here 2')
 
                 self.mu_star_df = df.groupby(level=['centre','param']).mean().stack().reorder_levels(order=[2,0,1]).sort_index()
                 self.mu_star_df.index.names = ['ts', 'centre', 'param']
+
+                print('here3')
 
                 self.mu_overall = df.apply(lambda x: np.mean(list(np.unique(x))))
                 self.var_overall = df.apply(lambda x: np.var(list(np.unique(x)), ddof=1))
@@ -1171,6 +1178,9 @@ class TSVARS(VARS):
                 self.sobol_value = tsvars_funcs.sobol_eq(variogram_value, e_covariogram_value, var_overall)
                 self.ivars = pd.DataFrame.from_dict({scale: self.variogram.groupby(level=['ts', 'param']).apply(tsvars_funcs.ivars, scale=scale, delta_h=self.delta_h) \
                       for scale in self.ivars_scales}, 'index')
+
+                print('here4')
+                self.run_status = True
 
 
         elif self.vars_eval_method == 'parallel':
@@ -1242,6 +1252,8 @@ class TSVARS(VARS):
                       for scale in self.ivars_scales}, 'index')
                     self.ivars = pd.concat([self.ivars_values, temp_ivars_values])
 
+                    self.run_status = True
+
             else:
                 # pair_df is built serially - other functions are the same as parallel
                 self.pair_df = applyParallel(a.groupby(level=0, axis=1), ts_pair)
@@ -1259,6 +1271,8 @@ class TSVARS(VARS):
                 self.sobol_value = tsvars_funcs.sobol_eq(variogram_value, e_covariogram_value, var_overall)
                 self.ivars = pd.DataFrame.from_dict({scale: self.variogram.groupby(level=['ts', 'param']).apply(tsvars_funcs.ivars, scale=scale, delta_h=self.delta_h) \
                       for scale in self.ivars_scales}, 'index')
+
+                self.run_status = True
 
 
     def temp_func(func, name, group):     
