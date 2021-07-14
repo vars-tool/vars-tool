34402d6d10d8261c74f390b41971a64b8128e255
Kasra Keshavarz <kasra.keshavarz@usask.ca>
2021-07-13 22:44:57 -0600

    correcting hbv sask

--- src/varstool/vars_sa.py
+++ src/varstool/vars_sa.py
@@ -1419,33 +1419,6 @@ class TSVARS(VARS):
                 }
 
     @staticmethod
-    @contextlib.contextmanager
-    def _tqdm_joblib(tqdm_object):
-        """
-        Context manager to patch joblib to report into tqdm progress bar given as argument
-        
-        Source: https://stackoverflow.com/questions/24983493
-                /tracking-progress-of-joblib-parallel-execution
-                /58936697#58936697
-        """
-        class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
-            def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs)
-
-            def __call__(self, *args, **kwargs):
-                tqdm_object.update(n=self.batch_size)
-                return super().__call__(*args, **kwargs)
-
-        old_batch_callback = joblib.parallel.BatchCompletionCallBack
-        joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
-        
-        try:
-            yield tqdm_object
-        finally:
-            joblib.parallel.BatchCompletionCallBack = old_batch_callback
-            tqdm_object.close()  
-
-    @staticmethod
     def _applyParallel(
         dfGrouped: pd.DataFrame, 
         func: Callable, 
@@ -1454,6 +1427,32 @@ class TSVARS(VARS):
 
         def _temp_func(func, name, group):
             return func(group), name
+
+        @contextlib.contextmanager
+        def _tqdm_joblib(tqdm_object):
+            """
+            Context manager to patch joblib to report into tqdm progress bar given as argument
+            
+            Source: https://stackoverflow.com/questions/24983493
+                    /tracking-progress-of-joblib-parallel-execution
+                    /58936697#58936697
+            """
+            class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+                def __init__(self, *args, **kwargs):
+                    super().__init__(*args, **kwargs)
+
+                def __call__(self, *args, **kwargs):
+                    tqdm_object.update(n=self.batch_size)
+                    return super().__call__(*args, **kwargs)
+
+            old_batch_callback = joblib.parallel.BatchCompletionCallBack
+            joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+            
+            try:
+                yield tqdm_object
+            finally:
+                joblib.parallel.BatchCompletionCallBack = old_batch_callback
+                tqdm_object.close()  
 
         if progress:
             with _tqdm_joblib(tqdm(desc="building pairs", total=len(dfGrouped))) as progress_bar:
