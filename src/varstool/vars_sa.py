e88c3332e3d5afe4cbb028fee1b6016d2e42f22d
Kasra Keshavarz <kasra.keshavarz@usask.ca>
2021-07-22 23:46:56 -0600

    TSVARS testing chunks

--- src/varstool/vars_sa.py
+++ src/varstool/vars_sa.py
@@ -1206,6 +1206,7 @@ class TSVARS(VARS):
                         temp_pair_df = df_temp.groupby(level=0, axis=1).progress_apply(ts_pair)
                     else:
                         temp_pair_df = df_temp.groupby(level=0, axis=1).apply(ts_pair)
+                    print(temp_pair_df)
                     temp_pair_df.index.names = ['centre', 'param', 'h', 'pair_ind']
                     temp_pair_df.columns.names = ['ts', None]
                     temp_pair_df = temp_pair_df.stack(level='ts').reorder_levels([-1,0,1,2,3]).sort_index()
