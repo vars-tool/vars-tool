b93cf461e2b6bdfc52bb4da72dfe25c78570fda1
Cordell Blanchard <CordellBlanchard@gmail.com>
2022-01-18 15:30:38 -0600

    Fixed Tasks 1,2, and 3 for basic VARS

--- src/varstool/vars_sa.py
+++ src/varstool/vars_sa.py
@@ -536,7 +536,7 @@ class VARS(object):
 
                 barfig.tight_layout()
 
-                if logy is True:
+                if logy:
                     barax.set_yscale('log')
 
                 plt.show()
@@ -717,8 +717,8 @@ class VARS(object):
             'IVARSub':self.ivarsub if self.bootstrap_flag is True else None,
             'relST':self.rel_st_factor_ranking if self.bootstrap_flag is True else None,
             'relIVARS':self.rel_ivars_factor_ranking if self.bootstrap_flag is True else None,
-            'Groups': [self.ivars50_grp, self.st_grp] if self.grouping_flag is True else None,
-            'relGrp': [self.reli_st_grp, self.reli_ivars50_grp] if self.grouping_flag is True else None,
+            'Groups': [self.ivars50_grp, self.st_grp] if ((self.grouping_flag is True) and (self.bootstrap_flag is True)) else None,
+            'relGrp': [self.reli_st_grp, self.reli_ivars50_grp] if ((self.grouping_flag is True) and (self.bootstrap_flag is True)) else None,
         }
 
         return
