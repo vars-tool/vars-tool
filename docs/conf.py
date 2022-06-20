d7c6bab03e508c5130f038c9e23466b1c5ff9e2a
Cordell Blanchard <CordellBlanchard@gmail.com>
2022-06-20 09:47:47 -0600

    Updating download requirements

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Saman Razavi'
 author = 'Saman Razavi'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.1'
+release = '2.3.2'
 
 
 # -- General configuration ---------------------------------------------------
